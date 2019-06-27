a9a78ff29094a2fbd77115ba6d18a1c0b1ac64c3
Mischa Meyer <m.meyer74@bluewin.ch>
2019-06-27 10:26:08 +0200

    wip DI implemented

---
+++ console/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using service;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            var services = new ServiceCollection();
+            services.AddTransient<Service2>();
+            services.AddTransient<Service1>();
+
+            var provider = services.BuildServiceProvider();
+
+            using (var service1 = provider.GetService<Service1>())
+            {
+                
+                // so something with the class
+            }
+        }
+
+
+    }
+}
---
+++ console/service/Service.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace service
+{
+    public class Service1 : IDisposable
+    {
+        private readonly Service2 _child;
+        public Service1(Service2 child)
+        {
+            Console.WriteLine("Constructor Service1");
+            _child = child;            
+        }
+
+        public object GetData() {
+            return null;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose Service1");
+            _child.Dispose();
+        }
+    }
+
+    public class Service2 : IDisposable
+    {
+        public Service2()
+        {
+            Console.WriteLine("Constructor Service2");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose Service2");
+        }
+    }
+}
